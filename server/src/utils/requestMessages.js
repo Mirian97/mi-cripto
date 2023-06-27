4c8e71a69503cdccb891552caa04c94de821aaa2
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 16:54:51 -0300

    feat: added new message to email already exists

--- server/src/utils/requestMessages.js
+++ server/src/utils/requestMessages.js
@@ -1,5 +1,6 @@
 module.exports = {
   internalServerError: 'Error interno del servidor',
   invalidEmailOrPassword: 'Correo electrónico o contraseña no válidos',
-  unauthorizedUser: 'Usuario no autorizado'
+  unauthorizedUser: 'Usuario no autorizado',
+  emailAlreadyExists: 'Este correo electrónico ya se está utilizando'
 }
