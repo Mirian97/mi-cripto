1666d4503034b4d0d66ea18f97391377a51e4f9c
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 07:17:44 -0300

    fix: import of routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const { routes } = require('./routes')
+const routes = require('./routes')
 const PORT = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(PORT, () => console.log(`Server is running in PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
