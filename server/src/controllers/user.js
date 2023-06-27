30d8b2b4eaf29ee2a8a0ee6bd37079ef5b87bb1e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 16:56:19 -0300

    fix: added validation to user email and added encripted password

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,8 +1,12 @@
+const { uploadImage, deleteImage } = require('../services/upload')
 const knex = require('../configs/connection')
 const bcrypt = require('bcrypt')
-const { invalidEmailOrPassword, internalServerError } = require('../utils/requestMessages')
 const saltOrRounds = 10
-const { uploadImage, deleteImage } = require('../services/upload')
+const {
+  invalidEmailOrPassword,
+  internalServerError,
+  emailAlreadyExists
+} = require('../utils/requestMessages')
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body
@@ -25,16 +29,23 @@ const detailUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   const { id, avatar_path } = req.loggedInUser
+  const { email, password } = req.body
+  let image = null
   try {
-    let image = null
+    const userEmailFound = await knex('users').first().where({ email })
+    if (userEmailFound && userEmailFound.id !== id) {
+      return res.status(400).json({ message: emailAlreadyExists })
+    }
     if (req.file) {
       const { originalname, mimetype, buffer } = req.file
       avatar_path && (await deleteImage(avatar_path))
       image = await uploadImage(`users/${id}/${originalname}`, buffer, mimetype)
     }
+    const encryptedPassword = await bcrypt.hash(password, saltOrRounds)
     const updatedUser = await knex('users')
       .update({
         ...req.body,
+        password: encryptedPassword,
         avatar_path: image ? image.path : null,
         avatar_url: image ? image.url : null
       })
@@ -42,6 +53,7 @@ const updateUser = async (req, res) => {
       .returning('*')
     return res.status(200).json(updatedUser[0])
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ message: internalServerError })
   }
 }
