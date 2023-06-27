76c7020cbbf8137fb4eadde88e3b0be1bd353494
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 13:22:25 -0300

    feat: added controller update user in routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const validateBody = require('./middlewares/validateBody')
 const validateLogin = require('./middlewares/validateLogin')
-const { registerUser, detailUser } = require('./controllers/user')
+const { registerUser, detailUser, updateUser } = require('./controllers/user')
 const login = require('./controllers/login')
+const multer = require('./middlewares/multer')
 const registerUserSchema = require('./schemas/registerUserSchema')
+const updateUserSchema = require('./schemas/updateUserSchema')
 const loginSchema = require('./schemas/loginSchema')
 const routes = express()
 
@@ -11,5 +13,6 @@ routes.post('/login', validateBody(loginSchema), login)
 routes.post('/user', validateBody(registerUserSchema), registerUser)
 routes.use(validateLogin)
 routes.get('/user', detailUser)
+routes.put('/user/:idUser', multer.single('avatar'), validateBody(updateUserSchema), updateUser)
 
 module.exports = routes
