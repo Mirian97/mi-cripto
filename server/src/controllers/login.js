76d5e399287a3d0c3ef76a099fe03f4ed4b22c12
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 09:45:17 -0300

    feat: created login user controller

---
+++ server/src/controllers/login.js
@@ -0,0 +1,27 @@
+const knex = require('../configs/connection')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { invalidEmailOrPassword, internalServerError } = require('../utils/requestMessages')
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const loggedUser = await knex('users').where({ email }).first()
+    if (!loggedUser) {
+      return res.status(400).json({ message: invalidEmailOrPassword })
+    }
+    const validPassword = await bcrypt.compare(password, loggedUser.password)
+    if (!validPassword) {
+      return res.status(400).json({ message: invalidEmailOrPassword })
+    }
+    const token = jwt.sign({ id: loggedUser.id }, process.env.JWT_PASSWORD, {
+      expiresIn: '8h'
+    })
+    const { password: _password, avatar: _avatar, ...userData } = loggedUser
+    return res.status(200).json({ ...userData, token })
+  } catch (error) {
+    return res.status(500).json({ message: internalServerError })
+  }
+}
+
+module.exports = login
