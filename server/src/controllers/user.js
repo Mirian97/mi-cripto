7773488647dfd5809c1dca8460b78d449699d983
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-27 13:21:47 -0300

    feat: created controller to update user

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -2,6 +2,7 @@ const knex = require('../configs/connection')
 const bcrypt = require('bcrypt')
 const { invalidEmailOrPassword, internalServerError } = require('../utils/requestMessages')
 const saltOrRounds = 10
+const { uploadImage, deleteImage } = require('../services/upload')
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body
@@ -22,7 +23,31 @@ const detailUser = async (req, res) => {
   return res.status(200).json(req.loggedInUser)
 }
 
+const updateUser = async (req, res) => {
+  const { id, avatar_path } = req.loggedInUser
+  try {
+    let image = null
+    if (req.file) {
+      const { originalname, mimetype, buffer } = req.file
+      avatar_path && (await deleteImage(avatar_path))
+      image = await uploadImage(`users/${id}/${originalname}`, buffer, mimetype)
+    }
+    const updatedUser = await knex('users')
+      .update({
+        ...req.body,
+        avatar_path: image ? image.path : null,
+        avatar_url: image ? image.url : null
+      })
+      .where({ id })
+      .returning('*')
+    return res.status(200).json(updatedUser[0])
+  } catch (error) {
+    return res.status(500).json({ message: internalServerError })
+  }
+}
+
 module.exports = {
   registerUser,
-  detailUser
+  detailUser,
+  updateUser
 }
