ad27346d6c75b78a3e9f0413d1d0e590b135fabe
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-01 12:13:37 -0300

    feat: created custom title component

---
+++ web/src/components/CustomTitle.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps(['title', 'textColor'])
+const { title, textColor } = toRefs(props)
+</script>
+
+<template>
+  <h1 class="title" :style="{ color: textColor }">{{ title }}</h1>
+</template>
+
+<style scoped>
+.title {
+  font-size: 40px;
+  font-family: 'Belanosima', sans-serif;
+}
+</style>
