9b60ca126a9d72055b8cae2762828b431e4f589e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-03 10:31:25 -0300

    feat: created service to login user

--- web/src/services/user.js
+++ web/src/services/user.js
@@ -4,3 +4,8 @@ export const registerUser = async (body) =>
   await api.post('/user', {
     ...body
   })
+
+export async function login(email, password) {
+  const { data } = await api.post('/login', { email, password })
+  return data
+}
