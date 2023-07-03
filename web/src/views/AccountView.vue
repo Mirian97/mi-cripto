ab09bab902d0dd1d04de8ef1e95bd5408374ce3f
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-03 18:04:28 -0300

    feat: created button to logout user

--- web/src/views/AccountView.vue
+++ web/src/views/AccountView.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from '../stores/user'
+const { removeUser } = useUserStore()
+</script>
 
 <template>
   <h1>Cuenta principal</h1>
+  <button @click="removeUser">Logout</button>
 </template>
