989742fd61cb07591dffe6132498fa5563df9294
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-03 18:03:39 -0300

    feat: added requiresAuth in account path and applied validate auth routes

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AccountView from '../views/AccountView.vue'
 import LoginView from '../views/LoginView.vue'
 import SignUpView from '../views/SignUpView.vue'
+import { validateAuthRoutes } from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,8 +17,17 @@ const router = createRouter({
       name: 'inscribir',
       component: SignUpView
     },
-    { path: '/cuenta', name: 'cuenta', component: AccountView }
+    {
+      path: '/cuenta',
+      name: 'cuenta',
+      component: AccountView,
+      meta: {
+        requiresAuth: true
+      }
+    }
   ]
 })
+
+router.beforeEach(validateAuthRoutes)
 
 export default router
