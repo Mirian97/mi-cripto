0f1a945cb7bdc80e745ab3b5762a778ef59cfc7a
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-06 12:50:27 -0300

    feat: implemented search crypto with delay

--- web/src/views/AccountView.vue
+++ web/src/views/AccountView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { debounce } from 'lodash'
+import { onMounted, ref, watchEffect } from 'vue'
 import Spinner from '../assets/spinner.svg'
 import CryptoCard from '../components/CryptoCard.vue'
 import CryptoModal from '../components/CryptoModal.vue'
@@ -7,22 +8,41 @@ import Header from '../components/Header.vue'
 import { listCrypto } from '../services/crypto'
 import { useUserStore } from '../stores/user'
 import { messageError } from '../utils/toast'
+
 const { token } = useUserStore()
 const cryptos = ref([])
+const search = ref('')
+const clearSearch = () => (search.value = '')
 
 const handleListCryptos = async () => {
   try {
-    const result = await listCrypto(token)
+    const result = await listCrypto(token, search.value)
     cryptos.value = result
   } catch (error) {
     messageError(error.response.data.message)
   }
 }
+watchEffect(() => {
+  search.value ? debounce(handleListCryptos, 500)() : handleListCryptos()
+})
 onMounted(() => handleListCryptos())
 </script>
 
 <template>
   <Header />
+  <v-sheet max-width="400" class="mx-auto mt-8 px-5">
+    <v-text-field
+      label="Buscar una criptomoneda..."
+      variant="solo-inverted"
+      maxLength="50"
+      v-model="search"
+      prepend-inner-icon="mdi-magnify"
+      append-inner-icon="mdi-currency-usd"
+      clear-icon="mdi-close-circle"
+      clearable
+      @click:clear="clearSearch"
+    />
+  </v-sheet>
   <div class="card-container">
     <CryptoCard
       v-if="cryptos.length"
@@ -42,12 +62,16 @@ onMounted(() => handleListCryptos())
   align-items: center;
   justify-content: center;
   gap: 40px;
-  padding: 40px 20px;
+  padding: 20px 20px 40px;
   max-width: 1440px;
   margin: auto;
 }
 
 .loading {
   margin-top: 100px;
+}
+
+.mdi-close {
+  cursor: pointer;
 }
 </style>
