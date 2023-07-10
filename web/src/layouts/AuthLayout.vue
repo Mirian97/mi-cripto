87bdc5a6b852303b495973e7e79a2be668685298
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-10 11:00:51 -0300

    feat: created auth layout to render account and profile pages

---
+++ web/src/layouts/AuthLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CryptoModal from '../components/CryptoModal.vue'
+import Header from '../components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="layout-content">
+    <slot name="content" />
+  </div>
+  <CryptoModal />
+</template>
+
+<style>
+.layout-content {
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px 40px;
+  max-width: 1380px;
+  margin: auto;
+}
+</style>
