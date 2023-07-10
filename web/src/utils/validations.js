3002442356d8279a2f416c450c5300107f97162d
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-10 20:51:48 -0300

    feat: added validate description max length

--- web/src/utils/validations.js
+++ web/src/utils/validations.js
@@ -1,7 +1,14 @@
-import { fieldRequired, invalidEmail, passwordDoesntMatch, passwordMinLength } from './messages'
+import {
+  descriptionMaxLength,
+  fieldRequired,
+  invalidEmail,
+  passwordDoesntMatch,
+  passwordMinLength
+} from './messages'
 
 export const validateFieldRequired = (value) => !!value || fieldRequired
 export const validateEmail = (value) => /.+@.+\..+/.test(value) || invalidEmail
 export const validatePasswordMinLength = (value) => value.length >= 5 || passwordMinLength
 export const validateMatchingPasswords = (value, currentPassword) =>
   value === currentPassword || passwordDoesntMatch
+export const validateDescriptionMaxLength = (value) => value.length < 501 || descriptionMaxLength
