a35923fb8f65e442699f81ef13b449a2d584f29e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-10 20:52:53 -0300

    feat: created description rules constant

--- web/src/utils/rules.js
+++ web/src/utils/rules.js
@@ -1,5 +1,11 @@
-import { validateEmail, validateFieldRequired, validatePasswordMinLength } from './validations'
+import {
+  validateDescriptionMaxLength,
+  validateEmail,
+  validateFieldRequired,
+  validatePasswordMinLength
+} from './validations'
 
 export const nameRules = [validateFieldRequired]
 export const emailRules = [validateFieldRequired, validateEmail]
 export const passwordRules = [validateFieldRequired, validatePasswordMinLength]
+export const descriptionRules = [validateDescriptionMaxLength]
