2a4b9652efa1e1bccf2df778334dfc573833fe81
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-11 16:45:04 -0300

    feat: created functions to validate image size and image extension

--- web/src/utils/validations.js
+++ web/src/utils/validations.js
@@ -1,7 +1,9 @@
 import {
   descriptionMaxLength,
   fieldRequired,
+  imageMaxSize1MB,
   invalidEmail,
+  invalidImageExtension,
   passwordDoesntMatch,
   passwordMinLength
 } from './messages'
@@ -12,3 +14,9 @@ export const validatePasswordMinLength = (value) => value.length >= 5 || passwor
 export const validateMatchingPasswords = (value, currentPassword) =>
   value === currentPassword || passwordDoesntMatch
 export const validateDescriptionMaxLength = (value) => value.length < 501 || descriptionMaxLength
+export const validateImageSize = (value) => value.size < 1024 * 1024 || imageMaxSize1MB
+export const validateImageExtension = (value) => {
+  const validExtensions = ['jpg', 'jpeg', 'png']
+  const extension = value.name.split('.').pop()
+  return validExtensions.includes(extension) || invalidImageExtension
+}
